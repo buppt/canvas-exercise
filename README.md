4f4eff7ea51e936a13c3eac5fcc685bddce233e9
618lab404 <574690129@qq.com>
2018-12-25 22:51:00 +0800

    add bar chart

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # canvas-charts
-canvas实现图表
+自己使用canvas实现的图表，<a href='https://buppt.github.io/canvas-charts/index.html'>点击这里在网页中查看效果</a>。
+
+
+## 柱状图
+<img src='./GIF.gif' />
--- barChart.js
+++ barChart.js
@@ -1,206 +1,195 @@
-class sBarChart{
-    constructor(canvas, data, options) {
-        this.canvas = document.getElementById(canvas);
+class BarChart{
+    constructor(id,data,options){
+        this.canvas = document.getElementById(id);
         this.ctx = this.canvas.getContext('2d');
-        this.data = data;                           // 存放图表数据
-        this.dataLength = this.data.length;         // 图表数据的长度
-        this.width = this.canvas.width;             // canvas 宽度
-        this.height = this.canvas.height;           // canvas 高度
-        this.padding = 50;                          // canvas 内边距
-        this.yEqual = 5;                            // y轴分成5等分
-        this.yLength = 0;                           // y轴坐标点之间的真实长度
-        this.xLength = 0;                           // x轴坐标点之间的真实长度
-        this.yFictitious = 0;                       // y轴坐标点之间显示的间距
-        this.yRatio = 0;                            // y轴坐标真实长度和坐标间距的比
-        this.bgColor = '#ffffff';                   // 默认背景颜色
-        this.fillColor = '#1E9FFF';                 // 默认填充颜色
-        this.axisColor = '#666666';                 // 坐标轴颜色
-        this.contentColor = '#eeeeee';              // 内容横线颜色
-        this.titleColor = '#000000';                // 图表标题颜色
-        this.title = '';                            // 图表标题
-        this.titlePosition = 'top';                 // 图表标题位置: top / bottom
-        this.looped = null;                         // 是否循环
-        this.current = 0;                           // 当前加载柱状图高度的百分数
-        this.currentIndex = -1;
-        this.onceMove = -1;
-        this.init(options);
+        this.data = data
+        this.width = this.canvas.width
+        this.height = this.canvas.height
+        this.title = options.title
+        this.padding = 50
+        this.yPart = 5
+        this.currentHeight = 0
+        this.backgroundColor = '#F3F3F3'    
+        this.titleColor = '#000000'
+        this.lineColor = '#CCCCCC'
+        this.xyColor = '#000'
+        this.chartColor = '#3398DB'
+        this.hoverColor = '#D7D7D7'
+        this.init()
     }
-
-    init(options) {
-        if(options){
-            this.padding = options.padding || 50;
-            this.yEqual = options.yEqual || 5;
-            this.bgColor = options.bgColor || '#ffffff';
-            this.fillColor = options.fillColor || '#1E9FFF';
-            this.axisColor = options.axisColor || '#666666';
-            this.contentColor = options.contentColor || '#eeeeee';
-            this.titleColor = options.titleColor || '#000000';
-            this.title = options.title;
-            this.titlePosition = options.titlePosition || 'top';
-        }
-        this.yLength = Math.floor((this.height - this.padding * 2 - 10) / this.yEqual);
-        this.xLength = Math.floor((this.width - this.padding * 1.5 - 10) / this.dataLength);
-        this.yFictitious = this.getYFictitious(this.data);
-        this.yRatio = this.yLength / this.yFictitious;
-        this.looping();
+    init(){
+        this.xWidth = this.width - this.padding*2
+        this.yHeight = this.height - this.padding*2
+        this.yOneHeight = this.yHeight / this.yPart
+        this.yOneNum = this.getYOneLen()
+        this.xOneWidth = this.xWidth / this.data.length
+        //this.draw()
+        this.animation()
     }
-    looping() {
-        this.looped = requestAnimationFrame(this.looping.bind(this));
-        if(this.current < 100){
-            this.current = (this.current + 3) > 100 ? 100 : (this.current + 3);
-            this.drawAnimation();
+    getYOneLen(){
+        var arr = this.data.slice(0)
+        arr.sort((a,b)=>b.value-a.value)
+        var yMax = arr[0].value
+        var yOne = yMax/this.yPart
+        var len = yOne.toString().length-1
+        len = len>2 ? 2: len
+        return Math.ceil(yOne/Math.pow(10,len))*Math.pow(10,len)
+    }
+    animation(){
+        this.loop = window.requestAnimationFrame(this.animation.bind(this))
+        if(this.currentHeight<100){
+            this.currentHeight = this.currentHeight+4>100?100: this.currentHeight+4
+            this.clearDraw()
+            this.draw()
         }else{
-            window.cancelAnimationFrame(this.looped);
-            this.looped = null;
-            this.watchHover();
+            window.cancelAnimationFrame(this.loop)
+            this.loop=null
+            this.watchHover()
         }
     }
-    drawAnimation() {
-        for(var i = 0; i < this.dataLength; i++) {
-            var x = Math.ceil(this.data[i].value * this.current / 100 * this.yRatio);
-            var y = this.height - this.padding - x;
-            
-            this.data[i].left = this.padding + this.xLength * (i + 0.25);
-            this.data[i].top = y;
-            this.data[i].right = this.padding + this.xLength * (i + 0.75);
-            this.data[i].bottom = this.height - this.padding;
-            this.drawUpdate();
-        }
-    }
-    drawUpdate() {
-        this.ctx.fillStyle = this.bgColor;
+    clearDraw(){
+        this.ctx.fillStyle = this.backgroundColor;       
         this.ctx.fillRect(0, 0, this.width, this.height);
-        this.drawAxis();
-        this.drawPoint();
-        this.drawTitle();
-        this.drawChart();
     }
-    drawChart() {
-        this.ctx.fillStyle = this.fillColor;
-        for(var i = 0; i < this.dataLength; i++) {
-            this.ctx.fillRect(
-                this.data[i].left, 
-                this.data[i].top, 
-                this.data[i].right - this.data[i].left, 
-                this.data[i].bottom - this.data[i].top
-            );
-            this.ctx.font = '12px Arial'
-            this.ctx.fillText(
-                this.data[i].value * this.current / 100,
-                this.data[i].left + this.xLength / 4, 
-                this.data[i].top - 15
-            );
-        }
-    }
-    drawAxis() {
-        this.ctx.beginPath();
-        this.ctx.strokeStyle = this.axisColor;
-        // y轴线, +0.5是为了解决canvas画1像素会显示成2像素的问题
-        this.ctx.moveTo(this.padding + 0.5, this.height - this.padding + 0.5);
-        this.ctx.lineTo(this.padding + 0.5, this.padding + 0.5);
-        // x轴线
-        this.ctx.moveTo(this.padding + 0.5, this.height - this.padding + 0.5);
-        this.ctx.lineTo(this.width - this.padding / 2 + 0.5, this.height - this.padding + 0.5);
-        this.ctx.stroke();
-    }
-    drawPoint() {
-        // x轴坐标点
-        this.ctx.beginPath();
-        this.ctx.font = '12px Microsoft YaHei';
-        this.ctx.textAlign = 'center';
-        this.ctx.fillStyle = this.axisColor; 
-        for(var i = 0; i < this.dataLength; i ++){
-            var xAxis = this.data[i].xAxis;
-            var xlen = this.xLength * (i + 1);
-            this.ctx.moveTo(this.padding + xlen + 0.5, this.height - this.padding + 0.5);
-            this.ctx.lineTo(this.padding + xlen + 0.5, this.height - this.padding + 5.5);
-            this.ctx.fillText(xAxis, this.padding + xlen - this.xLength / 2, this.height - this.padding + 15);
-        }
-        this.ctx.stroke();
-
-        // y轴坐标点
-        this.ctx.beginPath();
-        this.ctx.font = '12px Microsoft YaHei';
-        this.ctx.textAlign = 'right';
-        this.ctx.fillStyle = this.axisColor;
-        this.ctx.moveTo(this.padding + 0.5, this.height - this.padding + 0.5);
-        this.ctx.lineTo(this.padding - 4.5, this.height - this.padding + 0.5);
-        this.ctx.fillText(0, this.padding - 10, this.height - this.padding + 5);
-        for(var i=0; i < this.yEqual; i ++){
-            var y = this.yFictitious * (i + 1);
-            var ylen = this.yLength * (i + 1);
-            this.ctx.beginPath();
-            this.ctx.strokeStyle = this.axisColor;
-            this.ctx.moveTo(this.padding + 0.5, this.height - this.padding - ylen + 0.5);
-            this.ctx.lineTo(this.padding - 4.5, this.height - this.padding - ylen + 0.5);
-            this.ctx.stroke();
-            this.ctx.fillText(y,this.padding - 10, this.height - this.padding - ylen + 5);
-            this.ctx.beginPath();
-            this.ctx.strokeStyle = this.contentColor;
-            this.ctx.moveTo(this.padding + 0.5, this.height - this.padding - ylen + 0.5)
-            this.ctx.lineTo(this.width - this.padding / 2 + 0.5, this.height - this.padding - ylen+0.5);
-            this.ctx.stroke();
-        }
+    draw(){
+        this.drawTitle()
+        this.drawXY()
+        this.drawYText()
+        this.drawYLine()
+        this.drawXText()
+        this.drawChart()
     }
     drawTitle() {
         if(this.title){
             this.ctx.beginPath();
             this.ctx.textAlign = 'center';
-            this.ctx.fillStyle = this.titleColor;
             this.ctx.font = '16px Microsoft YaHei';
-            if(this.titlePosition === 'bottom' && this.padding >= 40){
-                this.ctx.fillText(this.title, this.width / 2, this.height - 5)
-            }else{
-                this.ctx.fillText(this.title, this.width / 2, this.padding / 2)
-            }
+            this.ctx.fillStyle = this.titleColor;      
+            this.ctx.fillText(this.title, this.width / 2, this.padding / 2)
         }
     }
-    /**
-     * 监听鼠标移动事件
-     */
+    drawXY(){
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = this.xyColor;   
+        this.ctx.moveTo(this.padding,this.padding-5)
+        this.ctx.lineTo(this.padding,this.height-this.padding)
+        this.ctx.lineTo(this.width-this.padding,this.height-this.padding)
+        this.ctx.stroke()
+    }
+    drawYText(){
+        this.ctx.beginPath();
+        this.ctx.textAlign = 'center';
+        this.ctx.font = '12px Microsoft YaHei';
+        this.ctx.fillStyle = this.xyColor;
+        var height = this.padding
+        var num = this.yOneNum * this.yPart
+        for(let i=0;i<this.yPart;i++){
+            this.ctx.fillText(num,this.padding/2,height+2)
+            height += this.yOneHeight
+            num -= this.yOneNum
+        }
+    }
+    drawYLine(){
+        this.ctx.beginPath();
+        this.ctx.strokeStyle  = this.lineColor;
+        var height = this.padding
+        for(let i=0;i<this.yPart;i++){
+            this.ctx.moveTo(this.padding,height)
+            this.ctx.lineTo(this.width-this.padding,height)
+            height += this.yOneHeight
+        }
+        this.ctx.stroke()
+    }
+    drawXText(){
+        this.ctx.beginPath();
+        this.ctx.textAlign = 'center';
+        this.ctx.font = '12px Microsoft YaHei';
+        this.ctx.fillStyle = this.xyColor;
+        var width = this.padding
+        var point = this.yHeight/(this.yOneNum*this.yPart)
+        var height = 0;
+        for(let i=0;i<this.data.length;i++){
+            height=this.data[i].value*point*this.currentHeight/100
+            this.ctx.fillText(this.data[i].x,width+ this.xOneWidth/2,this.height-this.padding/2)
+            width += this.xOneWidth
+        }
+    }
+    drawChart(){
+        this.ctx.beginPath();
+        this.ctx.fillStyle = this.chartColor;
+        var width = this.padding
+        var point = this.yHeight/(this.yOneNum*this.yPart)
+        var height = 0;
+        for(let i=0;i<this.data.length;i++){
+            height=this.data[i].value*point*this.currentHeight/100
+            this.ctx.fillRect(
+                width+this.xOneWidth/6,
+                this.height-this.padding-height,
+                this.xOneWidth*2/3,
+                height,
+            );
+            this.ctx.fillText(this.data[i].value,width+ this.xOneWidth/2,this.height-this.padding-height-1)
+            width += this.xOneWidth
+        }
+    }
+    
     watchHover() {
         var self = this;
         self.canvas.addEventListener('mousemove', function(ev) {
             ev = ev || window.event;
             self.currentIndex = -1;
+            var width = self.padding
             for (var i = 0; i < self.data.length; i ++){
-                if( ev.offsetX > self.data[i].left &&
-                    ev.offsetX < self.data[i].right &&
-                    ev.offsetY > self.data[i].top &&
-                    ev.offsetY < self.data[i].bottom )
+                if(ev.offsetX > self.padding+self.xOneWidth*i &&
+                ev.offsetX <self.padding+self.xOneWidth*(i+1) &&
+                ev.offsetY <self.padding+self.height &&
+                ev.offsetY > self.padding
+                )
                 {
                     self.currentIndex = i;
                 }
+                width += this.xOneWidth
             }
             self.drawHover();
+        })
+        self.canvas.addEventListener('click', function(ev) {
+            ev = ev || window.event;
+            self.currentIndex = -1;
+            var width = self.padding
+            for (var i = 0; i < self.data.length; i ++){
+                if(ev.offsetX > self.padding+self.xOneWidth*i &&
+                ev.offsetX <self.padding+self.xOneWidth*(i+1) &&
+                ev.offsetY <self.padding+self.height &&
+                ev.offsetY > self.padding
+                )
+                {
+                    self.currentIndex = i;
+                }
+                width += this.xOneWidth
+            }
+            alert(self.data[self.currentIndex].value)
         })
     }
     drawHover() {
         if(this.currentIndex !== -1){
-            if(this.onceMove === -1){
+            if(this.onceMove !== this.currentIndex){
                 this.onceMove = this.currentIndex;
-                this.canvas.style.cursor = 'pointer';
+                this.clearDraw()
+                this.ctx.fillStyle = this.hoverColor
+                this.ctx.fillRect(
+                    this.padding+this.onceMove*this.xOneWidth,
+                    this.padding,
+                    this.xOneWidth,
+                    this.yHeight,
+                );
+                this.draw()
             }
         }else{
             if(this.onceMove !== -1){
+                this.clearDraw()
+                this.draw()
                 this.onceMove = -1;
-                this.canvas.style.cursor = 'inherit';
             }
         }
-    }
-    /**
-     * y轴坐标点之间显示的间距
-     * @param data 
-     * @return y轴坐标间距
-     */
-    getYFictitious(data) {
-        var arr = data.slice(0);
-        arr.sort(function(a,b){
-            return -(a.value-b.value);
-        });
-        var len = Math.ceil(arr[0].value / this.yEqual);
-        var pow = len.toString().length - 1;
-        pow = pow > 2 ? 2 : pow;
-        return Math.ceil(len / Math.pow(10,pow)) * Math.pow(10,pow);
     }
 }
